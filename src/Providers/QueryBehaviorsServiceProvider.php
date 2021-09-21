fda5d20c28a3d60f973b8ce13cfaf963f19f7217
Hossein <hosseinsheikh.web@gmail.com>
2021-09-21 19:43:36 +0430

    initial package

---
+++ src/Providers/QueryBehaviorsServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace HosseinSheikh\QueryBehaviors\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class QueryBehaviorsServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot() {
+    }
+}
